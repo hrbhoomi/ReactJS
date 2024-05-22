8112cbe0b1e7bafb2084c0134fe4e7e3304a8ec3
hrbhoomi <hrbhoomi2003@gmail.com>
2024-05-22 21:46:43 +0530

    feature sample code working

---
+++ my-react-app-day1/src/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+
+    return(
+
+        <header>
+            <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#About">About</a></li>
+                    <li><a href="#Services">Services</a></li>
+                    <li><a href="#Contact">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+export default Header
